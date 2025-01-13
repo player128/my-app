6fbdcf48698e66579396b8d3439b7a1ecf4705ff
Slava <slavamonster6@gmail.com>
2025-01-13 15:10:26 +0300

    add fetch data and create 2 hooks

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
-import React, {useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import './styles/App.css'
 import { PostList } from "./components/PostList";
 import { PostForm } from "./components/PostFrom";
 import { PostFilter } from "./components/PostFilter";
 import { MyModal } from "./components/UI/MyModal/MyModal";
 import { MyButton } from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import { Loader } from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id : 1, title : 'B', body : 'C'},
-    {id : 2, title : 'Z', body : 'A'},
-    {id : 3, title : 'A', body : 'B'},
-  ]);
-
+  console.log('render');
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort : '', query : ''});
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  let pagesArray = getPagesArray(totalPages);
 
-  const sortedPosts = useMemo(() => {
-    console.log('Функция SortedPosts');
-    if (filter.sort) {
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }, [filter.sort, posts]);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit));
+  });
+  console.log(pagesArray);
 
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query));
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
+
+  console.log(totalPages);
+
+  useEffect(() => {
+    fetchPosts();
+  }, [])
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id));
@@ -50,7 +59,13 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>
+      {
+        postError &&  <h1>Произошла ошибка {postError}</h1>
+      }
+      {isPostsLoading 
+      ? <div style={{display : 'flex', justifyContent : 'center', marginTop : 50}}><Loader/></div>
+      :  <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>}
+      {pagesArray.map()}
     </div>
   );
 }
---
+++ src/hooks/useFetching.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export const useFetching = (callback) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+    
+    const fethcing = async () => {
+        try {
+            setIsLoading(true);
+            await callback();
+            
+        } catch(e) {
+            setError(e.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return [fethcing, isLoading, error];
+}
